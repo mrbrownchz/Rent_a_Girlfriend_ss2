aacecbc582d1f75ecc5ff6b3b92f6b4367817da6
mrbrownchz <nutchaphon121@gmail.com>
2022-08-06 19:19:01 +0700

    update

--- js/loader.js
+++ js/loader.js
@@ -3,5 +3,8 @@ window.addEventListener("load" , () => {
     setTimeout(() => {
         loader.style.opacity = 0;
         document.body.style.overflow = "auto";
+        setTimeout(() => {
+            loader.style.display = "none";
+        },300)
     },1000)
 })
